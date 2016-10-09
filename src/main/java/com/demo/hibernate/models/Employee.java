d742504efc0f19dd830e00cee5311f5afbb4766e
hobbes09 <sourin.sutradhar@gmail.com>
2016-10-09 17:50:58 +0530

    model class

---
+++ src/main/java/com/demo/hibernate/models/Employee.java
@@ -0,0 +1,42 @@
+package com.demo.hibernate.models;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee implements Serializable{
+	public Employee() {
+
+	};
+
+	public Employee(String name) {
+		this.name = name;
+
+	};
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
